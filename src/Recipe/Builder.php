ec78761f9c65741bd70b12546b13ceb06aa1f534
Jabar Asadi <asadi.jabar@outlook.com>
2018-09-25 12:49:43 +0330

    event dispatcher

---
+++ src/Recipe/Builder.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\ExposeApi\Recipe;
+
+use App\ExposeApi\Recipe\Core\Event\AbstractRecipeEvent;
+use Closure;
+
+class Builder
+{
+
+    /**
+     * Create a new Recipe based on Template
+     *
+     * @param  $data
+     * @param  Closure $callback
+     * @return string
+     */
+    public function create($data, Closure $callback = null): string
+    {
+
+        return $this->build(
+            plug(
+                $this->createTemplate($data), function ($template) use ($callback) {
+
+                    return $callback($template);
+
+                }
+            ), "RecipeCreated"
+        );
+    }
+
+    /**
+     * Delete a Recipe template
+     *
+     * @param  $id
+     * @param  Closure $callback
+     * @return string
+     */
+    public function delete(array $id, Closure $callback = null): string
+    {
+        return $this->build($this->createTemplate($id), "RecipeDeleted");
+    }
+
+    /**
+     * Update the Recipe with given data
+     *
+     * @param  $data
+     * @param  Closure $callback
+     * @return string
+     */
+    public function update(array $data, Closure $callback = null): string
+    {
+
+        return $this->build(
+            plug(
+                $this->createTemplate($data), function ($template) use ($callback) {
+
+                    return $callback($template);
+
+                }
+            ), "RecipeUpdated"
+        );
+    }
+
+    /**
+     * Get one or more Recipe[s] details
+     *
+     * @param  $id
+     * @param  Closure|null $callback
+     * @return mixed
+     */
+    public function get(array $id, Closure $callback = null)
+    {
+        return $this->build($this->createTemplate($id), "RecipeQueried");
+    }
+
+    /**
+     * Rate the Recipe
+     *
+     * @param  array   $data
+     * @param  Closure $callback
+     * @return mixed
+     */
+    public function rate(array $data, Closure $callback)
+    {
+        return $this->build(
+            plug(
+                $this->createTemplate($data), function ($template) use ($callback) {
+
+                    return $callback($template);
+
+                }
+            ), "RecipeRated"
+        );
+    }
+
+    /**
+     * Search the Recipe
+     *
+     * @param  array   $data
+     * @param  Closure $callback
+     * @return mixed
+     */
+    public function search(array $data, Closure $callback)
+    {
+        return $this->build(
+            plug(
+                $this->createTemplate($data), function ($template) use ($callback) {
+
+                    return $callback($template);
+
+                }
+            ), "RecipeSearched"
+        );
+    }
+
+    /**
+     * dispatch the event
+     *
+     * @param  RecipeTemplate $templateObject
+     * @return mixed
+     */
+    private function build(RecipeTemplate $templateObject, $event)
+    {
+
+        return dispatch(AbstractRecipeEvent::getContextFromType($event), $templateObject);
+    }
+
+    /**
+     * Create new Template
+     *
+     * @param  array $data
+     * @return RecipeTemplate
+     */
+    public function createTemplate(array $data): RecipeTemplate
+    {
+        return new RecipeTemplate($data);
+    }
+
+}
---
+++ src/Recipe/RecipeTemplate.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\ExposeApi\Recipe;
+
+
+use App\ExposeApi\Core\Fluent;
+use ArrayIterator;
+
+final class RecipeTemplate implements \IteratorAggregate
+{
+
+    /**
+     * the name of recipe
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     * unique id for recipe
+     *
+     * @var int
+     */
+    public $id;
+
+    /**
+     * the prepare time
+     *
+     * @var string
+     */
+    public $prepTime;
+
+    /**
+     * level of difficulty
+     *
+     * @var int
+     */
+    public $difficulty;
+
+    /**
+     * is recipe can be used by vegetarian or not
+     *
+     * @var bool
+     */
+    public $vegetarian;
+
+    /**
+     * Representation of Recipe template data
+     *
+     * @var Fluent
+     */
+    private $fluent;
+
+    /**
+     * Rate of recipe (1-5)
+     *
+     * @var
+     */
+    public $rate;
+
+
+    /**
+     * RecipeTemplate constructor.
+     *
+     * @param  $data
+     * @param  Fluent $fluent
+     * @throws \Exception
+     */
+    public function __construct(array $data, Fluent $fluent = null)
+    {
+        $this->fluent = $fluent ?? (new Fluent($data));
+    }
+
+    /**
+     * Dynamic value assignments
+     *
+     * @param  $name
+     * @param  $args
+     * @return RecipeTemplate
+     */
+    public function __call($name, $args)
+    {
+        if (property_exists($this, $name)) {
+
+            if (isset($this->fluent->toArray()[$name])) {
+                $this->{$name} = $this->fluent->toArray()[$name];
+
+            }
+            if (!empty($args[0])) {
+                $this->{$name} = $args[0];
+                $this->fluent->append($name, $args[0]);
+
+            }
+        }
+
+        if ($name == 'id' && !empty($this->id)) {
+            $this->fluent->append('id', $this->id);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Fluent|array
+     */
+    public function getFluent()
+    {
+        return $this->fluent;
+    }
+
+
+    /**
+     * @inheritdoc
+     *
+     * return Traversable object from RecipeTemplate class
+     */
+    public function getIterator(): ArrayIterator
+    {
+        return new ArrayIterator($this->getFluent()->toArray());
+    }
+
+}
